e5dfd971cc4a18aec15dfaa55bdaeaeb5c84805a
Marcos <mmarkito708@gmail.com>
2024-12-04 23:01:41 +0100

    Home page text done

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import { Box, Typography, Button, Grid } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Typography, Button, Dialog, DialogContent, Grid } from '@mui/material';
 import { styled } from '@mui/system';
 import Carousel from 'react-material-ui-carousel';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import CloseIcon from '@mui/icons-material/Close';
 
 const styles = {
   heroSection: {
@@ -40,6 +43,12 @@ const styles = {
     color: '#E30613',
     textAlign: 'center',
   },
+  sectionSecondTitle: {
+    fontWeight: 'bold',
+    marginBottom: '20px',
+    color: '#666',
+    textAlign: 'center',
+  },
   video: {
     width: '100%',
     height: 'auto',
@@ -52,6 +61,15 @@ const styles = {
     backgroundColor: '#f9f9f9',
     borderRadius: '8px',
     boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+  },
+  imageHome: {
+    width: '90%',
+    height: '500px',
+    alignItems: 'center',
+    marginTop: '1px',
+    marginLeft: '60px',
+    marginRight: '50px',
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
   },
 };
 
@@ -74,7 +92,101 @@ const carouselItems = [
   },
 ];
 
+// Estilización del diálogo para que abarque toda la pantalla
+const StyledDialog = styled(Dialog)(() => ({
+    '& .MuiDialog-paper': {
+      backgroundColor: 'rgba(0, 0, 0, 0.8)',
+      color: '#fff',
+      width: '100%',
+      height: '100vh',
+      margin: 0,
+      maxWidth: '100%',
+      maxHeight: 'none',
+      borderRadius: 0,
+      padding: '20px',
+      position: 'relative',
+    },
+    '& .MuiBackdrop-root': {
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+  }));
+  
+  const CloseButton = styled(Button)({
+    position: 'absolute',
+    top: '10px',
+    right: '10px',
+    color: '#fff',
+    '&:hover': {
+      backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    },
+  });
+  
+  const NavigationButton = styled(Button)(({ theme }) => ({
+    position: 'absolute',
+    top: '50%',
+    transform: 'translateY(-50%)',
+    color: 'white',
+    '&:hover': {
+      backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    },
+  }));
+  
+  const PrevButton = styled(NavigationButton)({
+    left: '10px',
+  });
+  
+  const NextButton = styled(NavigationButton)({
+    right: '10px',
+  });
+
+// Datos de los estilos de cocinas
+const kitchenStyles = [
+    {
+      name: 'Modern',
+      images: ['/Images/modern.png', '/Images/modern.png', '/Images/modern.png', '/Images/modern.png', '/Images/modern.png'],
+      coverImage: '/Images/1.jpg',
+    },
+    {
+      name: 'Classic',
+      images: ['/Images/1.jpg', '/Images/2.jpg', '/Images/1.jpg', '/Images/2.jpg', '/Images/1.jpg'],
+      coverImage: '/Images/1.jpg',
+    },
+    {
+      name: 'European',
+      images: ['/Images/1.jpg', '/Images/2.jpg', '/Images/3.jpg', '/Images/4.jpg', '/Images/1.jpg'],
+      coverImage: '/Images/1.jpg',
+    },
+    {
+      name: 'Unique',
+      images: ['/Images/1.jpg', '/Images/2.jpg', '/Images/3.jpg', '/Images/1.jpg', '/Images/4.jpg'],
+      coverImage: '/Images/1.jpg',
+    },
+  ];
+
 const HomePage = () => {
+
+    const [openPopup, setOpenPopup] = useState(false);
+  const [currentImages, setCurrentImages] = useState([]);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handleOpenPopup = (images) => {
+    setCurrentImages(images);
+    setCurrentImageIndex(0);
+    setOpenPopup(true);
+  };
+
+  const handleClosePopup = () => {
+    setOpenPopup(false);
+  };
+
+  const handleNext = () => {
+    setCurrentImageIndex((current) => (current + 1) % currentImages.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentImageIndex((current) => (current - 1 + currentImages.length) % currentImages.length);
+  };
+
   return (
     <Box>
       {/* Hero Section with Carousel */}
@@ -107,7 +219,7 @@ const HomePage = () => {
             },
           }}
         >
-          Learn More
+          More Info
         </Button>
       </Box>
     </Box>
@@ -118,25 +230,47 @@ const HomePage = () => {
       {/* Introduction Section */}
       <Box sx={{ padding: '40px', textAlign: 'center' }}>
         <Typography variant="h4" sx={styles.sectionTitle}>
-          Welcome to Dller Kitchens
+        Supplying the luxury Kitchen industry for Over 35 Years
         </Typography>
-        <Typography variant="body1" sx={{ color: '#666', maxWidth: '800px', margin: '0 auto' }}>
-          At Dller Kitchens, we believe that every kitchen tells a story. Our mission is to create custom-made kitchens
-          that not only meet your needs but also reflect your personality. From design to execution, we deliver
-          exceptional quality and timeless elegance.
+        <Typography variant="h5" sx={styles.sectionSecondTitle}>
+        "Your Trusted Partner in Creating Handmade Kitchens, Combining Timeless Craftsmanship with Modern Innovation."
+        <br />
+        </Typography>
+        <Typography variant="body1" sx={{ color: '#666', maxWidth: '90%', margin: '0 auto', textAlign: 'left' }}>
+            <br />
+        We pride ourselves on confidentiality, we have been making furniture for some of Europe’s most exclusive brands for over 3 decades, combining their creativity with our craftsmanship to deliver exceptional Kitchen creations. 
+        <br />
+        <br />
+We work in partnership with a small number of Kitchen companies to take the worry out of production, delivering exceptional quality, so you can focus on delivering your customers exceptional service. 
+
         </Typography>
       </Box>
 
-      {/* Video Section */}
-      <Box sx={{ padding: '40px', textAlign: 'center' }}>
-        <Typography variant="h4" sx={styles.sectionTitle}>
-          Watch Our Story
+      {/* Image Section */}
+      <Box 
+                component="img" 
+                src="/Images/modern.png" 
+                alt="Excellence in Design" 
+                sx={styles.imageHome} 
+              />
+               <Typography variant="body1" sx={{ color: '#666', maxWidth: '90%', margin: '0 auto', textAlign: 'left' }}>
+            <br />
+            <strong>Experts in Handmade Excellence</strong>
+            <br />
+            At <strong>Dller</strong> we specialize in crafting custom-made wooden kitchens tailored to your clients' exact specifications. With over three decades of expertise, a team of 40+ highly skilled carpenters, and a commitment to quality, we deliver bespoke kitchens to your designs with our superior craftsmanship.
+
         </Typography>
-        <video controls style={styles.video}>
-          <source src="/Images/video.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </Box>
+
+           {/* Video Section */}
+      <Box sx={{ padding: '40px', textAlign: 'center' }}>
+            <Typography variant="h4" sx={styles.sectionTitle}>
+            Watch Our Story
+            </Typography>
+            <video controls style={styles.video}>
+            <source src="/Images/video.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+            </video>
+        </Box>
 
       {/* Grid Section */}
       <Box sx={{ padding: '40px' }}>
@@ -152,10 +286,70 @@ const HomePage = () => {
                 style={{ width: '100%', borderRadius: '8px', marginBottom: '20px' }}
               />
               <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
-                Unmatched Quality
+              Expertise
               </Typography>
               <Typography variant="body1">
-                Our kitchens are built with precision and care to ensure the highest quality.
+              35 years of creating bespoke kitchens.
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box sx={styles.gridSection}>
+              <img
+                src="/Images/1.jpg"
+                alt="Innovation"
+                style={{ width: '100%', borderRadius: '8px', marginBottom: '20px' }}
+              />
+              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+              Skilled Craftsmanship
+              </Typography>
+              <Typography variant="body1">
+              A team of over 40 dedicated carpenters and designers.
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box sx={styles.gridSection}>
+              <img
+                src="/Images/3.jpg"
+                alt="Sustainability"
+                style={{ width: '100%', borderRadius: '8px', marginBottom: '20px' }}
+              />
+              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+              Modern Technology
+              </Typography>
+              <Typography variant="body1">
+              Precision ensured with state-of-the-art tools.
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box sx={styles.gridSection}>
+              <img
+                src="/Images/1.jpg"
+                alt="Innovation"
+                style={{ width: '100%', borderRadius: '8px', marginBottom: '20px' }}
+              />
+              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+              Tailored Solutions
+              </Typography>
+              <Typography variant="body1">
+              Kitchens designed to meet your unique designs.
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box sx={styles.gridSection}>
+              <img
+                src="/Images/1.jpg"
+                alt="Innovation"
+                style={{ width: '100%', borderRadius: '8px', marginBottom: '20px' }}
+              />
+              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+              Competitive Pricing:
+              </Typography>
+              <Typography variant="body1">
+              Working in partnership to meet your needs.
               </Typography>
             </Box>
           </Grid>
@@ -174,23 +368,73 @@ const HomePage = () => {
               </Typography>
             </Box>
           </Grid>
-          <Grid item xs={12} md={4}>
-            <Box sx={styles.gridSection}>
-              <img
-                src="/Images/3.jpg"
-                alt="Sustainability"
-                style={{ width: '100%', borderRadius: '8px', marginBottom: '20px' }}
-              />
-              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
-                Sustainability
-              </Typography>
-              <Typography variant="body1">
-                We use eco-friendly materials and sustainable processes in every project.
-              </Typography>
-            </Box>
-          </Grid>
         </Grid>
       </Box>
+{/* Show case seccion*/}
+
+      <Typography variant="h5" sx={styles.sectionSecondTitle}>
+      Explore Our Craftsmanship
+        </Typography>
+
+        <Box>
+        <Box display="flex" flexDirection="row" flexWrap="wrap" justifyContent="center">
+  {kitchenStyles.map((style, index) => (
+    <Button
+      key={index}
+      onClick={() => handleOpenPopup(style.images)}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        margin: 20,
+        padding: '10px',
+        backgroundColor: '#fff', // Fondo blanco para el botón
+        boxShadow: '0 2px 4px rgba(0,0,0,0.1)', // Sombra ligera para el botón
+        borderRadius: '8px', // Bordes redondeados para todo el botón
+        width: '20%', // Anchura fija para cada botón
+      }}
+    >
+      <img
+        src={style.coverImage}
+        alt={style.name}
+        style={{
+          width: '100%', // Asegura que la imagen cubra el ancho del botón
+          height: '220px', // Altura fija para la imagen
+          objectFit: 'cover', // Asegura que la imagen cubra el espacio asignado sin deformarse
+          borderRadius: '8px 8px 0 0', // Redondea las esquinas superiores
+        }}
+      />
+      <Typography
+        component="span" // Usa span para el texto dentro del botón
+        style={{
+          fontWeight: 'bold', // Texto en negrita
+          marginTop: '10px', // Espacio entre la imagen y el texto
+          textAlign: 'center', // Centra el texto
+          color: 'red',
+        }}
+      >
+        {style.name}
+      </Typography>
+    </Button>
+  ))}
+</Box>
+
+
+      <StyledDialog open={openPopup} onClose={handleClosePopup}>
+        <DialogContent>
+          <img src={currentImages[currentImageIndex]} alt="Showcase image" style={{ width: '100%', height: '80vh', objectFit: 'contain' }} />
+          <PrevButton onClick={handlePrev}><ChevronLeftIcon /></PrevButton>
+          <NextButton onClick={handleNext}><ChevronRightIcon /></NextButton>
+          <CloseButton onClick={handleClosePopup}><CloseIcon /></CloseButton>
+        </DialogContent>
+      </StyledDialog>
+    </Box>
+
+    <Typography variant="h5" sx={styles.sectionSecondTitle}>
+        <br />
+        <br />
+    "IF YOU CAN IMAGINE IT WE CAN MAKE IT”
+        </Typography>
     </Box>
   );
 };
